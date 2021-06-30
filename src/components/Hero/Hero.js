e36c3506b7b1287206bc64eab581e48353f3bc78
habib <habibdev96@gmail.com>
2021-06-29 20:32:24 -0600

    created hero & added bg pattern to hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,7 +1,21 @@
 import './Hero.scss';
+import { PrimaryButton, SecondaryButton } from '../Buttons/Buttons';
 
 const Hero = () => {
-  return <div></div>;
+  return (
+    <div className='hero'>
+      <div className='hero-content'>
+        <h1 className='hero-heading'> A modern publishing platform</h1>
+        <p className='hero-desc'>
+          Grow your audience and build your online brand
+        </p>
+        <div className='hero-btns'>
+          <PrimaryButton text='Start for Free' />
+          <SecondaryButton text='Learn More' />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
